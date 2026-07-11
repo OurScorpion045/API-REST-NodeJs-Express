44eaa7136d77bd5a86e6b55a5491bc45ddafee6c
OurScorpion045 <sams24484@gmail.com>
2026-07-10 20:08:13 -0700

    Correction: File index.js

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const app = express();
 
 const port = process.env.PORT ?? 3000;
 
-const server = http.createServer(app);
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
