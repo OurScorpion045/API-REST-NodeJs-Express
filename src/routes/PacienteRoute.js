9afa9adf60e25c408dfe205cba4bebde951eb4ac
OurScorpion045 <sams24484@gmail.com>
2026-07-14 16:12:41 -0700

    Update: File PacienteRoute.js

---
+++ src/routes/PacienteRoute.js
@@ -0,0 +1,24 @@
+import { PacienteController } from "../controllers/PacienteController.js";
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/pacientes", (req, res) => {
+    await PacienteController.getAll();
+});
+
+router.get("/pacientes/:id", (req, res) => {
+    await PacienteController.getById(req, res);
+});
+
+router.post("/pacientes", (req, res) => {
+    await PacienteController.insert(req, res);
+});
+
+router.put("/pacientes/:id", (req, res) => {
+    await PacienteController.update(req, res);
+});
+
+router.delete("/pacientes/:id", (req, res) => {
+    await PacienteController.delete(req, res);
+});
