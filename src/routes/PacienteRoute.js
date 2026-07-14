0537f31176dfa389281b4903921a339dd933df7d
OurScorpion045 <sams24484@gmail.com>
2026-07-14 16:44:41 -0700

    Correction: File PacienteRoute.js

--- src/routes/PacienteRoute.js
+++ src/routes/PacienteRoute.js
@@ -1,24 +1,24 @@
 import { PacienteController } from "../controllers/PacienteController.js";
 import { express } from "express";
 
-export const router = express.Router();
+export const pacientesRouter = express.Router();
 
-router.get("/pacientes", (req, res) => {
+pacientesRouter.get("/pacientes", (req, res) => {
     await PacienteController.getAll();
 });
 
-router.get("/pacientes/:id", (req, res) => {
+pacientesRouter.get("/pacientes/:id", (req, res) => {
     await PacienteController.getById(req, res);
 });
 
-router.post("/pacientes", (req, res) => {
+pacientesRouter.post("/pacientes", (req, res) => {
     await PacienteController.insert(req, res);
 });
 
-router.put("/pacientes/:id", (req, res) => {
+pacientesRouter.put("/pacientes/:id", (req, res) => {
     await PacienteController.update(req, res);
 });
 
-router.delete("/pacientes/:id", (req, res) => {
+pacientesRouter.delete("/pacientes/:id", (req, res) => {
     await PacienteController.delete(req, res);
 });
