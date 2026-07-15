1290bf99f4db6efb12c776d5a245b68f288522b4
OurScorpion045 <sams24484@gmail.com>
2026-07-15 13:59:22 -0700

    Correction: File PacienteController.js

--- src/controllers/PacienteController.js
+++ src/controllers/PacienteController.js
@@ -1,4 +1,3 @@
-import { json } from "express";
 import { PacienteModel } from "../models/PacienteModel.js";
 
 export class PacienteController {
@@ -8,7 +7,7 @@ export class PacienteController {
             let results = await PacienteModel.getAll();
             res.json(results);
         } catch (err) {
-            res.json({error: "Error al seleccionar pacientes"});
+            res.json({"message": "Error al seleccionar pacientes"});
             console.error(err);
         }
 
@@ -16,75 +15,75 @@ export class PacienteController {
 
     static async getById(req, res) {
         try {
-            let data = {PacienteId: req.params.id};
+            let data = [req.params.id];
             let results = await PacienteModel.getById(data);
             res.json(results);
         } catch (err) {
-            res.json({error: "Error al seleccionar paciente"});
+            res.json({"message": "Error al seleccionar paciente"});
             console.error(err);
         }
     }
 
     static async insert(req, res) {
         try {
-            let data = {
-                DNI: req.params.DNI,
-                Nombre: req.params.Nombre,
-                Direccion: req.params.Direccion,
-                CodigoPostal: req.params.CodigoPostal,
-                Telefono: req.params.Telefono,
-                Genero: req.params.Genero,
-                FechaNacimiento: req.params.FechaNacimiento,
-                Correo: req.params.Correo
-            }
+            let data = [
+                req.params.DNI,
+                req.params.Nombre,
+                req.params.Direccion,
+                req.params.CodigoPostal,
+                req.params.Telefono,
+                req.params.Genero,
+                req.params.FechaNacimiento,
+                req.params.Correo
+            ]
             let results = await PacienteModel.insert(data);
             if (results.affectedRows > 0) {
-                res.json({message: "Paciente insertado correctamente"});
+                res.json({"message": "Paciente insertado correctamente"});
             } else {
                 throw new Error(err);
             }
         } catch (err) {
-            res.json({error: "Error al insertar paciente"});
+            res.json({"message": "Error al insertar paciente"});
             console.error(err);
         }
     }
 
     static async update(req, res) {
         try {
-            let data = {
-                DNI: req.params.DNI,
-                Nombre: req.params.Nombre,
-                Direccion: req.params.Direccion,
-                CodigoPostal: req.params.CodigoPostal,
-                Telefono: req.params.Telefono,
-                Genero: req.params.Genero,
-                FechaNacimiento: req.params.FechaNacimiento,
-                Correo: req.params.Correo,
-                PacienteId: req.params.id
-            }
+            let data = [
+                req.params.DNI,
+                req.params.Nombre,
+                req.params.Direccion,
+                req.params.CodigoPostal,
+                req.params.Telefono,
+                req.params.Genero,
+                req.params.FechaNacimiento,
+                req.params.Correo,
+                req.params.id
+            ]
             let results = await PacienteModel.update(data);
             if (results.affectedRows > 0) {
-                res.json({message: "Paciente actualizado correctamente"});
+                res.json({"message": "Paciente actualizado correctamente"});
             } else {
                 throw new Error(err);
             }
         } catch (err) {
-            res.json({error: "Error al actualizar paciente"});
+            res.json({"message": "Error al actualizar paciente"});
             console.error(err);
         }
     }
 
     static async delete(req, res) {
         try {
-            let data = {PacienteId: req.params.id};
+            let data = [req.params.id];
             let results = await PacienteModel.delete(data);
             if (results.affectedRows > 0) {
-                res.json({message: "Paciente eliminado correctamente"});
+                res.json({"message": "Paciente eliminado correctamente"});
             } else {
                 throw new Error(err);
             }
         } catch (err) {
-            res.json({error: "Error al eliminar paciente"});
+            res.json({"message": "Error al eliminar paciente"});
             console.error(err);
         }
     }
