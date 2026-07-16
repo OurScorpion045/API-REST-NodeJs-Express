995c6263051c0cc19f7247ff594b43d027d89449
OurScorpion045 <sams24484@gmail.com>
2026-07-16 12:36:44 -0700

    Update: File index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { pacientesRouter } from "./src/routes/PacienteRoute.js";
 import { citasRouter } from "./src/routes/CitasRoute.js";
+import { usuarioRouter } from "./src/routes/UsuarioRoute.js";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(pacientesRouter);
 app.use(citasRouter);
+app.use(usuarioRouter);
 
 const PORT = process.env.PORT ?? 3000;
 
