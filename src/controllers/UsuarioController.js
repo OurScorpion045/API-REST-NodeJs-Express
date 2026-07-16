5bf42d319315d31399fe466c04b00e0fd73c2800
OurScorpion045 <sams24484@gmail.com>
2026-07-16 12:24:09 -0700

    Update: File UsuarioController.js

---
+++ src/controllers/UsuarioController.js
@@ -0,0 +1,82 @@
+import { UsuarioModel } from "../models/UsuarioModel.js";
+import bcrypt from "bcrypt";
+
+export class UsuarioController {
+
+    static async getAll(req, res) {
+        try {
+            const results = await UsuarioModel.getAll();
+            res.json(results);
+        } catch (err) {
+            res.json({"message": "Error al seleccionar usuarios"});
+            console.error(err);
+        }
+    }
+
+    static async getById(req, res) {
+        try {
+            const data = [req.params.id];
+            const results = await UsuarioModel.getById(data);
+            res.json(results);
+        } catch (err) {
+            res.json({"message": "Error al seleccionar usuario"});
+            console.error(err);
+        }
+    }
+
+    static async insert(req, res) {
+        try {
+            const hashPassword = await bcrypt.hash(req.body.Password, 10);
+            const data = [
+                req.body.Usuario,
+                hashPassword,
+                req.body.Estado
+            ];
+            const results = await UsuarioModel.insert(data);
+            if (results.affectedRows > 0) {
+                res.json({"message": "Usuario insertado correctamente"});
+            } else {
+                throw new Error(err);
+            }
+        } catch (err) {
+            res.json({"message": "Error al insertar usuario"});
+            console.error(err);
+        }
+    }
+
+    static async update(req, res) {
+        try {
+            const hashPassword = await bcrypt.hash(req.body.Password, 10);
+            const data = [
+                req.body.Usuario,
+                hashPassword,
+                req.body.Estado,
+                req.params.id
+            ];
+            const results = await UsuarioModel.update(data);
+            if (results.affectedRows > 0) {
+                res.json({"message": "Usuario actualizado correctamente"});
+            } else {
+                throw new Error(err);
+            }
+        } catch (err) {
+            res.json({"message": "Error al actualizar usuario"});
+            console.error(err);
+        }
+    }
+
+    static async delete(req, res) {
+        try {
+            const data = [req.params.id];
+            const results = await UsuarioModel.delete(data);
+            if (results.affectedRows > 0) {
+                res.json({"message": "Usuario eliminado correctamente"});
+            } else {
+                throw new Error(err);
+            }
+        } catch (err) {
+            res.json({"message": "Error al eliminar usuario"});
+            console.error(err);
+        }
+    }
+}
