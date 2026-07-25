b48633d76d894aa90abde717ae25ed417af97ce5
OurScorpion045 <sams24484@gmail.com>
2026-07-25 16:13:41 -0700

    Update: File index.spec.js

--- src/test/index.spec.js
+++ src/test/index.spec.js
@@ -1,10 +1,19 @@
 import supertest from "supertest";
-import { app } from "../../index.js";
+import { app } from "../app.js"
 
 describe('GET /pacientes', () => {
 
-    test('Should respoind with a 200 status code', async () => {
+    test('Should respond with a 200 status code and the JSON patients data', async () => {
         const response = await supertest(app).get('/pacientes').send()
-        console.log(response);
+        expect(response.statusCode).toBe(200);
+        expect(response.type).toMatch(/json/);
+        expect(Array.isArray(response.body).toBe(true));
     });
-})
+
+    test('Should respond with a 200 status code and the JSON patient data register by the id', async() => {
+        const response = await supertest(app).get('/pacientes/:id').send()
+        expect(response.statusCode).toBe(200);
+        expect(response.type).toMatch(/json/);
+        expect(Array.isArray(response.body)).toBe(true);
+    });
+});
