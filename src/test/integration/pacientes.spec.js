96de8a2adc9d9670a9bc0054e4d3d148e69af9a9
OurScorpion045 <sams24484@gmail.com>
2026-08-03 12:47:54 -0700

    Update: File pacientes.spec.js

--- src/test/integration/pacientes.spec.js
+++ src/test/integration/pacientes.spec.js
@@ -1,7 +1,45 @@
 import supertest from "supertest";
 import { app } from "../../app.js"
+import { pool } from "../../config/Database.js";
 
-let id = null;
+
+describe('POST /pacientes', () => {
+    
+    test('Should respond with a 200 status code and a message in JSON about the patient was inserted correctly', async() => {
+        await pool.execute("DELETE FROM `pacientes`");
+        await pool.execute("ALTER TABLE `pacientes` AUTO_INCREMENT = 1");
+        const response = await supertest(app).post('/pacientes').send({
+            DNI: "F000000012",
+            Nombre: "Martin Lopez",
+            Direccion: "Calle de pruebas",
+            CodigoPostal: "20012",
+            Telefono: "623281567",
+            Genero: "H",
+            FechaNacimiento: "1987-11-30",
+            Correo: "Paciente@gmail.com"
+        })
+        expect(response.statusCode).toBe(200);
+        expect(response.type).toMatch(/json/);
+    });
+});
+
+describe(`PUT /pacientes/1`, () => {
+
+    test('Should respond with a 200 status code and a message in JSON about the patient was updated correctly', async() => {
+        const response = await supertest(app).put('/pacientes/1').send({
+            DNI: "F000000013",
+            Nombre: "Martin Lopez",
+            Direccion: "Calle de pruebas 12",
+            CodigoPostal: "20012",
+            Telefono: "623281567",
+            Genero: "H",
+            FechaNacimiento: "1987-11-30",
+            Correo: "Paciente12@gmail.com"
+        })
+        expect(response.statusCode).toBe(200);
+        expect(response.type).toMatch(/json/);
+    });
+});
 
 describe('GET /pacientes', () => {
 
@@ -20,50 +58,11 @@ describe('GET /pacientes', () => {
     });
 });
 
-describe('POST /pacientes', () => {
-    
-    test('Should respond with a 200 status code and a message in JSON about the patient was inserted correctly', async() => {
-        const response = await supertest(app).post('/pacientes').send({
-            DNI: "F000000012",
-            Nombre: "Martin Lopez",
-            Direccion: "Calle de pruebas 12",
-            CodigoPostal: "20012",
-            Telefono: "623281567",
-            Genero: "H",
-            FechaNacimiento: "1987-11-30",
-            Correo: "Paciente12@gmail.com"
-        })
-        expect(response.statusCode).toBe(200);
-        expect(response.type).toMatch(/json/);
-    });
-});
-
-describe(`PUT /pacientes/${id}`, () => {
-
-    test('Should respond with a 200 status code and a message in JSON about the patient was updated correctly', async() => {
-        const response = await supertest(app).put('/pacientes/:id').send({
-            DNI: "F000000013",
-            Nombre: "Martin Lopez",
-            Direccion: "Calle de pruebas 12",
-            CodigoPostal: "20012",
-            Telefono: "623281567",
-            Genero: "H",
-            FechaNacimiento: "1987-11-30",
-            Correo: "Paciente12@gmail.com"
-        })
-        expect(response.statusCode).toBe(200);
-        expect(response.type).toMatch(/json/);
-        expect(response.body).toMatch(/message/);
-        expect(response.body.DNI).toMatch(/F000000013/);
-    });
-});
-
-describe(`DELETE /pacientes/${id}`, () => {
+describe(`DELETE /pacientes/1`, () => {
 
     test('Should respond with a 200 status code and a message in JSON about the patient was deleted correctly', async() => {
-        const response = await supertest(app).delete('/pacientes/:id').send()
+        const response = await supertest(app).delete('/pacientes/1').send()
         expect(response.statusCode).toBe(200);
         expect(response.type).toMatch(/json/);
-        expect(response.body).toMatch(/message/);
     })
 })
