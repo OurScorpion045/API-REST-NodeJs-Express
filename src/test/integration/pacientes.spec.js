765b4739f24126e45440e605e251d901c847e0b1
OurScorpion045 <sams24484@gmail.com>
2026-08-04 13:57:17 -0700

    update: File pacientes.spec.js

--- src/test/integration/pacientes.spec.js
+++ src/test/integration/pacientes.spec.js
@@ -2,7 +2,7 @@ import supertest from "supertest";
 import { app } from "../../app.js"
 import { pool } from "../../config/Database.js";
 
-beforeAll(async () => {
+beforeEach(async () => {
     await pool.execute("DELETE FROM `pacientes`");
     await pool.execute("ALTER TABLE `pacientes` AUTO_INCREMENT = 1");
     await pool.execute("INSERT INTO `pacientes`(`DNI`, `Nombre`, `Direccion`, `CodigoPostal`, `Telefono`, `Genero`, `FechaNacimiento`, `Correo`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", ["F000000012", "Jon Doe", "Calle de pruebas 1", "20001", "1234567890", "H", "2001-01-01", "Paciente01@gmail.com"]);
@@ -28,7 +28,7 @@ describe('POST /pacientes', () => {
             Correo: "Paciente@gmail.com"
         })
         expect(response.statusCode).toBe(200);
-        expect(response.type).toBe(/json/);
+        expect(response.type).toMatch(/json/);
         expect(response.body.message).toBe("Paciente insertado correctamente");
     });
 });
