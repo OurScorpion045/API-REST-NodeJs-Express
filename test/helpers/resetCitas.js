f60fbea84fe123a93d401946fb64d64eba121c1e
OurScorpion045 <sams24484@gmail.com>
2026-08-06 17:18:04 -0700

    Update: File helpers/resetCitas.js

---
+++ test/helpers/resetCitas.js
@@ -0,0 +1,8 @@
+import { pool } from "../../src/config/Database";
+import { citaArray } from "../fixtures/cita.js";
+
+export const resetCitas = async () => {
+    await pool.execute("DELETE FROM `citas`");
+    await pool.execute("ALTER TABLE `citas` AUTO_INCREMENT = 1");
+    await pool.execute("INSERT INTO `citas`(`PacienteId`, `Fecha`, `HoraInicio`, `HoraFin`, `Estado`, `Motivo`) VALUES (?, ?, ?, ?, ?, ?)", citaArray);
+}
