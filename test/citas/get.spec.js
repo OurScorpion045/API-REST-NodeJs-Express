3ac1f709f24783497cb327d4deea1e68dd7bc14a
OurScorpion045 <sams24484@gmail.com>
2026-08-06 17:15:47 -0700

    Update: File citas/get.spec.js

---
+++ test/citas/get.spec.js
@@ -0,0 +1,29 @@
+import supertest from "supertest";
+import { app } from "../../src/app.js";
+import { citaJson } from "../fixtures/cita.js";
+import { resetCitas } from "../helpers/resetCitas.js";
+import { closeConnection } from "../helpers/closeConnection.js";
+
+beforeEach(resetCitas);
+
+afterAll(closeConnection);
+
+const request = supertest(app);
+
+describe("GET /citas", () => {
+
+    test("Should responde with a status code 200 and the array of the citas written in a json format", async () => {
+        const response = await request.get("/citas");
+        expect(response.statusCode).toBe(200);
+        expect(response.type).toMatch(/json/);
+        expect(Array.isArray(response.body)).toBe(true);
+        expect(response.body.length).toBeGreaterThan(0);
+    });
+
+    test("Should responde with a status code 200 and the cita's information written in json format according the id", async () => {
+        const response = await request.get("/citas/1");
+        expect(response.statusCode).toBe(200);
+        expect(response.type).toMatch(/json/);
+        expect(response.body.length).toBeGreaterThan(0);
+    });
+});
