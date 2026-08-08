906358f46f2749b7a96624aeaeec1d047971fc0b
OurScorpion045 <sams24484@gmail.com>
2026-08-07 20:03:45 -0700

    Update: File test/usuarios/post.spec.js

---
+++ test/usuarios/post.spec.js
@@ -0,0 +1,21 @@
+import supertest from "supertest";
+import { app } from "../../src/app.js";
+import { resetUsuarios } from "../helpers/resetUsuarios.js";
+import { closeConnection } from "../helpers/closeConnection.js";
+import { usuarioJson } from "../fixtures/usuario";
+
+const request = supertest(app);
+
+beforeEach(resetUsuarios);
+
+afterAll(closeConnection);
+
+describe("POST /usuarios/", () => {
+
+    test("Should return a 200 status code and a json message which says 'Usuario insertado correctamente'", async () => {
+        const response = await request.post("/usuarios").send(usuarioJson);
+        expect(response.statusCode).toBe(200);
+        expect(response.type).toMatch(/json/);
+        expect(response.body.message).toBe("Usuario insertado correctamente");
+    })
+})
