a2e185025e1dc7b3d9b434f64753b8bee8b638d4
OurScorpion045 <sams24484@gmail.com>
2026-08-07 17:33:05 -0700

    Update: File /test/fixtures/cita.js

--- test/fixtures/cita.js
+++ test/fixtures/cita.js
@@ -7,4 +7,13 @@ export const citaJson = {
     Motivo: "Dolor en el pie"
 }
 
+export const citaJsonUpdate = {
+    PacienteId: "1",
+    Fecha: "2020-06-18",
+    HoraInicio: "09:00:00",
+    HoraFin: "09:30:00",
+    Estado: "Confirmada",
+    Motivo: "Dolor de muelas"
+}
+
 export const citaArray = ["1", "2020-06-18", "09:00:00", "09:30:00", "Confirmada", "Dolor en el pie"]
