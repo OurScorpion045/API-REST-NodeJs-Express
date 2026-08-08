bab49e50e70ccb67aa405cedf9cd888733170e50
OurScorpion045 <sams24484@gmail.com>
2026-08-07 17:33:54 -0700

    Update: File /test/citas/post.spec.js

---
+++ test/citas/post.spec.js
@@ -0,0 +1,21 @@
+import supertest from "supertest";
+import { app } from "../../src/app.js";
+import { resetCitas } from "../helpers/resetCitas.js";
+import { closeConnection } from "../helpers/closeConnection.js";
+import { citaJson } from "../fixtures/cita.js";
+
+const request = supertest(app);
+
+beforeEach(resetCitas);
+
+afterAll(closeConnection);
+
+describe("POST /citas", () => {
+
+    test("Should return a 200 status code and a json message which says 'cita insertada correctamente'", async () => {
+        const response = await request.post("/citas").send(citaJson);
+        expect(response.statusCode).toBe(200);
+        expect(response.body.message).toBe("Cita insertada correctamente");
+        expect(response.type).toMatch(/json/);
+    });
+});
