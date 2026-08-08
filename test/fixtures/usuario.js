7235c0d8d89a42913118735d0114d31a970c5854
OurScorpion045 <sams24484@gmail.com>
2026-08-07 20:00:19 -0700

    Update: File test/fixtures/usuario.js

---
+++ test/fixtures/usuario.js
@@ -0,0 +1,13 @@
+export const usuarioJson = {
+    Usuario: "usuario1@gmail.com",
+    Password: "usuario1",
+    Estado: "Inactivo"
+};
+
+export const usuarioJsonUpdate = {
+    Usuario: "usuario10@gmail.com",
+    Password: "usuario2",
+    Estado: "Activo"
+}
+
+export const usuarioArray = ["usuario1@gmail.com", "usuario1", "Inactivo"];
