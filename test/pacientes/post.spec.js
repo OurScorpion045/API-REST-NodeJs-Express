dab7539ebdf55711a444524ecfe6aad0766d547a
OurScorpion045 <sams24484@gmail.com>
2026-08-13 14:53:07 -0700

    Update file test/pacientes/post.spec.js

--- test/pacientes/post.spec.js
+++ test/pacientes/post.spec.js
@@ -14,7 +14,7 @@ describe('POST /pacientes', () => {
     
     test('Should respond with a 200 status code and a message in JSON about the patient was inserted correctly', async() => {
         const response = await request.post('/pacientes').send(paciente);
-        expect(response.statusCode).toBe(200);
+        expect(response.statusCode).toBe(201);
         expect(response.type).toMatch(/json/);
         expect(response.body.message).toBe("Paciente insertado correctamente");
     });
