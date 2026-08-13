f2aca2637c293f4cfdbc5a8cad772cf100f2521d
OurScorpion045 <sams24484@gmail.com>
2026-08-13 14:51:06 -0700

    Correction: File PacienteController.js

--- src/controllers/PacienteController.js
+++ src/controllers/PacienteController.js
@@ -37,8 +37,10 @@ export class PacienteController {
                 req.body.Correo
             ]
             let results = await PacienteModel.insert(data);
-            if (results.affectedRows > 0) {
-                res.json({"message": "Paciente insertado correctamente"});
+            if (req.body.DNI == null || req.body.Nombre == null || req.body.Direccion == null || req.body.CodigoPostal == null || req.body.Telefono == null || req.body.Genero == null || req.body.FechaNacimiento == null || req.body.Correo == null) {
+                res.status(400).json({"message": "Paciente no encontrado"});
+            } else if (results.affectedRows > 0) {
+                res.status(201).json({"message": "Paciente insertado correctamente"});
             } else {
                 throw new Error(err);
             }
@@ -62,8 +64,12 @@ export class PacienteController {
                 req.params.id
             ]
             let results = await PacienteModel.update(data);
-            if (results.affectedRows > 0) {
-                res.json({"message": "Paciente actualizado correctamente"});
+            if (req.body.DNI == null || req.body.Nombre == null || req.body.Direccion == null || req.body.CodigoPostal == null || req.body.Telefono == null || req.body.Genero == null || req.body.FechaNacimiento == null || req.body.Correo == null) {
+                res.status(400).json({"message": "Paciente no encontrado"});
+            } else if (results.affectedRows == 0) {
+                res.status(404).json({"message": "Paciente no encontrado"});
+            } else if (results.affectedRows > 0) {
+                res.status(200).json({"message": "Paciente actualizado correctamente"});
             } else {
                 throw new Error(err);
             }
@@ -77,8 +83,10 @@ export class PacienteController {
         try {
             let data = [req.params.id];
             let results = await PacienteModel.delete(data);
-            if (results.affectedRows > 0) {
-                res.json({"message": "Paciente eliminado correctamente"});
+            if (results.affectedRows == 0) {
+                res.status(404).json({"message": "Paciente no encontrado"});
+            } else if (results.affectedRows > 0) {
+                res.status(204).send();
             } else {
                 throw new Error(err);
             }
