212cb2c92a29205ed7516698dec7240b37ab5711
OurScorpion045 <sams24484@gmail.com>
2026-08-13 14:52:22 -0700

    Update

--- test/citas/delete.spec.js
+++ test/citas/delete.spec.js
@@ -13,8 +13,7 @@ describe("DELETE /citas/:id", () => {
 
     test('Should return a 200 status code and a json message which says "Cita eliminada correctamente"', async () => {
         const response = await request.delete('/citas/1');
-        expect(response.statusCode).toBe(200);
-        expect(response.type).toMatch(/json/);
-        expect(response.body.message).toBe("Cita eliminada correctamente");
+        expect(response.statusCode).toBe(204);
+        expect(response.body.message).toBe();
     });
 });
