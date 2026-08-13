a9db9d288a8e7cca2641e56b2ff712d8f0032b3e
OurScorpion045 <sams24484@gmail.com>
2026-08-13 14:51:27 -0700

    Correction: File UsuarioController.js

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,3 +1,4 @@
+import { json } from "express";
 import { UsuarioModel } from "../models/UsuarioModel.js";
 import bcrypt from "bcrypt";
 
@@ -6,7 +7,7 @@ export class UsuarioController {
     static async getAll(req, res) {
         try {
             const results = await UsuarioModel.getAll();
-            res.json(results);
+            res.status(200).json(results);
         } catch (err) {
             res.status(500).json({"message": "Error al seleccionar usuarios"});
             console.error(err);
@@ -17,7 +18,12 @@ export class UsuarioController {
         try {
             const data = [req.params.id];
             const results = await UsuarioModel.getById(data);
-            res.json(results);
+
+            if (JSON.stringify(results) === "[]") {
+                res.status(404).json({"message": "Usuario no encontrado"});
+            } else {
+                res.status(200).json(results);
+            }
         } catch (err) {
             res.status(500).json({"message": "Error al seleccionar usuario"});
             console.error(err);
@@ -33,8 +39,10 @@ export class UsuarioController {
                 req.body.Estado
             ];
             const results = await UsuarioModel.insert(data);
-            if (results.affectedRows > 0) {
-                res.json({"message": "Usuario insertado correctamente"});
+            if (req.body.Usuario == null || hashPassword == null || req.body.Estado == null) {
+                res.status(400).json({"message": "Campos obligatorios vacios"});
+            } else if (results.affectedRows > 0) {
+                res.status(201).json({"message": "Usuario insertado correctamente"});
             } else {
                 throw new Error(err);
             }
@@ -53,9 +61,13 @@ export class UsuarioController {
                 req.body.Estado,
                 req.params.id
             ];
-            const results = await UsuarioModel.update(data);    
-            if (results.affectedRows > 0) {
-                res.json({"message": "Usuario actualizado correctamente"});
+            const results = await UsuarioModel.update(data);
+            if (req.body.Usuario == null || hashPassword == null || req.body.Estado == null) {
+                res.status(400).json({"message": "Campos obligatorios vacios"});
+            } else if (results.affectedRows == 0) {
+                res.status(404).json({"message": "Usuario no encontrado"});
+            } else if (results.affectedRows > 0) {
+                res.status(200).json({"message": "Usuario actualizado correctamente"});
             } else {
                 throw new Error(err);
             }
@@ -69,8 +81,11 @@ export class UsuarioController {
         try {
             const data = [req.params.id];
             const results = await UsuarioModel.delete(data);
-            if (results.affectedRows > 0) {
-                res.json({"message": "Usuario eliminado correctamente"});
+            if (results.affectedRows == 0) {
+                res.status(404).json({"message": "Usuario no encontrado"});
+            }
+            else if (results.affectedRows > 0) {
+                res.status(204).send();
             } else {
                 throw new Error(err);
             }
