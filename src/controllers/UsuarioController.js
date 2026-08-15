88301b797cea94f513cf9fd4bb58247fe9ca2138
OurScorpion045 <sams24484@gmail.com>
2026-08-14 23:56:17 -0700

    File UsuarioController.js

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -38,14 +38,15 @@ export class UsuarioController {
                 hashPassword,
                 req.body.Estado
             ];
-            const results = await UsuarioModel.insert(data);
-            if (req.body.Usuario == null || hashPassword == null || req.body.Estado == null) {
+            if (req.body.Usuario == '' || hashPassword == '' || req.body.Estado == '') {
                 res.status(400).json({"message": "Campos obligatorios vacios"});
-            } else if (results.affectedRows > 0) {
-                res.status(201).json({"message": "Usuario insertado correctamente"});
-            } else {
-                throw new Error(err);
             }
+            
+            const results = await UsuarioModel.insert(data);
+            
+            if (results.affectedRows > 0) {
+                res.status(201).json({"message": "Usuario insertado correctamente"});
+            } 
         } catch (err) {
             res.status(500).json({"message": "Error al insertar usuario"});
             console.error(err);
@@ -61,16 +62,17 @@ export class UsuarioController {
                 req.body.Estado,
                 req.params.id
             ];
-            const results = await UsuarioModel.update(data);
-            if (req.body.Usuario == null || hashPassword == null || req.body.Estado == null) {
+            if (req.body.Usuario == '' || hashPassword == '' || req.body.Estado == '') {
                 res.status(400).json({"message": "Campos obligatorios vacios"});
-            } else if (results.affectedRows == 0) {
+            } 
+            
+            const results = await UsuarioModel.update(data);
+            
+            if (results.affectedRows == 0) {
                 res.status(404).json({"message": "Usuario no encontrado"});
             } else if (results.affectedRows > 0) {
                 res.status(200).json({"message": "Usuario actualizado correctamente"});
-            } else {
-                throw new Error(err);
-            }
+            } 
         } catch (err) {
             res.status(500).json({"message": "Error al actualizar usuario"});
             console.error(err);
@@ -86,8 +88,6 @@ export class UsuarioController {
             }
             else if (results.affectedRows > 0) {
                 res.status(204).send();
-            } else {
-                throw new Error(err);
             }
         } catch (err) {
             res.status(500).json({"message": "Error al eliminar usuario"});
