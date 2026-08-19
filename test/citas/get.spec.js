7d04e9db9236c26a3193e5cc2fd826e53d587d73
OurScorpion045 <sams24484@gmail.com>
2026-08-18 22:38:14 -0700

    Update: File test/citas/get.spec.js

--- test/citas/get.spec.js
+++ test/citas/get.spec.js
@@ -20,10 +20,17 @@ describe("GET /citas", () => {
         expect(response.body.length).toBeGreaterThan(0);
     });
 
-    test("Should responde with a status code 200 and the cita's information written in json format according the id", async () => {
+    test("Should respond with a status code 200 and the cita's information written in json format according the id", async () => {
         const response = await request.get("/citas/1");
         expect(response.statusCode).toBe(200);
         expect(response.type).toMatch(/json/);
         expect(response.body.length).toBeGreaterThan(0);
+    });
+
+    test("Should respond with a status code 404 and a JSON message which says 'Cita no encontrada'", async () => {
+        const response = await request.get("/citas/999");
+        expect(response.statusCode).toBe(404);
+        expect(response.type).toMatch(/json/);
+        expect(response.body.message).toBe("Cita no encontrada");
     });
 });
