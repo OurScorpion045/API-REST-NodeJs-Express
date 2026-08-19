b92df798ff1003efb66890d2900aedf35ef9d5d0
OurScorpion045 <sams24484@gmail.com>
2026-08-18 22:39:04 -0700

    Update: File test/citas/put.spec.js

--- test/citas/put.spec.js
+++ test/citas/put.spec.js
@@ -2,7 +2,7 @@ import supertest from "supertest";
 import { app } from "../../src/app.js";
 import { resetCitas } from "../helpers/resetCitas.js";
 import { closeConnection } from "../helpers/closeConnection.js";
-import { citaJsonUpdate } from "../fixtures/cita.js";
+import { citaJsonUpdate, citaJsonEmpty } from "../fixtures/cita.js";
 
 const request = supertest(app);
 
@@ -18,4 +18,18 @@ describe("PUT /citas/:id", () => {
         expect(response.type).toMatch(/json/);
         expect(response.body.message).toBe("Cita actualizada correctamente");
     });
+
+    test('should return a 400 status code and a JSON format message which says "Campos obligatorios vacios"', async () => {
+        const response = await request.put("/citas/1").send(citaJsonEmpty);
+        expect(response.statusCode).toBe(400);
+        expect(response.type).toMatch(/json/);
+        expect(response.body.message).toBe("Campos obligatorios vacios");
+    });
+
+    test('should return a 404 status code and a JSON format message which says "Cita no encontrada"', async () => {
+        const response = await request.put("/citas/999").send(citaJsonUpdate);
+        expect(response.statusCode).toBe(404);
+        expect(response.type).toMatch(/json/);
+        expect(response.body.message).toBe("Cita no encontrada");
+    })
 });
