7cc22bc46990d65734734a7d0a44706b687ad57f
OurScorpion045 <sams24484@gmail.com>
2026-08-22 23:25:17 -0700

    Update: File test/fixtures/paciente.js

--- test/fixtures/paciente.js
+++ test/fixtures/paciente.js
@@ -1,10 +1,21 @@
 export const paciente = {
-            DNI: "F000000012",
-            Nombre: "Martin Lopez",
-            Direccion: "Calle de pruebas",
-            CodigoPostal: "20012",
-            Telefono: "623281567",
-            Genero: "H",
-            FechaNacimiento: "1987-11-30",
-            Correo: "Paciente@gmail.com"
-        }
+    DNI: "F000000012",
+    Nombre: "Martin Lopez",
+    Direccion: "Calle de pruebas",
+    CodigoPostal: "20012",
+    Telefono: "623281567",
+    Genero: "H",
+    FechaNacimiento: "1987-11-30",
+    Correo: "Paciente@gmail.com"
+}
+
+export const pacienteEmpty = {
+    DNI: "",
+    Nombre: "",
+    Direccion: "",
+    CodigoPostal: "",
+    Telefono: "",
+    Genero: "",
+    FechaNacimiento: "",
+    Correo: ""
+}
