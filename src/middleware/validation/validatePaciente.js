0ae983a688104e11c09a3291e4d1b823f19d48d2
OurScorpion045 <sams24484@gmail.com>
2026-08-25 15:28:44 -0700

    Update: File validatePaciente.js

---
+++ src/middleware/validation/validatePaciente.js
@@ -0,0 +1,14 @@
+import { pacienteSchema } from "../../schemas/paciente.schema.js";
+
+export const validatePaciente = (req, res, next) => {
+    const validate = pacienteSchema.safeParse(req.body);
+
+    if (!validate.success) {
+        return res.status(400).json({
+            error: "Datos invalidos",
+            detalles: validate.error.issues
+        });
+    }
+
+    next();
+};
