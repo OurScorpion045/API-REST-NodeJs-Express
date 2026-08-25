0f276f0f01f99adc8d8ce64b06e792c9be4377ec
OurScorpion045 <sams24484@gmail.com>
2026-08-25 15:28:22 -0700

    Update: File PacienteController.js

--- src/controllers/PacienteController.js
+++ src/controllers/PacienteController.js
@@ -5,7 +5,7 @@ export class PacienteController {
     static async getAll(req, res) {
         try {
             let results = await PacienteModel.getAll();
-            res.json(results);
+            return res.json(results);
         } catch (err) {
             res.status(500).json({"message": "Error al seleccionar pacientes"});
             console.error(err);
@@ -18,9 +18,9 @@ export class PacienteController {
             let data = [req.params.id];
             let results = await PacienteModel.getById(data);
             if (JSON.stringify(results) === '[]') {
-                res.status(404).json({"message": "Paciente no encontrado"});
+                return res.status(404).json({"message": "Paciente no encontrado"});
             } else {
-                res.status(200).json(results);
+                return res.status(200).json(results);
             }
         } catch (err) {
             res.status(500).json({"message": "Error al seleccionar paciente"});
@@ -40,14 +40,11 @@ export class PacienteController {
                 req.body.FechaNacimiento,
                 req.body.Correo
             ]
-            if (req.body.DNI == '' || req.body.Nombre == '' || req.body.Direccion == '' || req.body.CodigoPostal == '' || req.body.Telefono == '' || req.body.Genero == '' || req.body.FechaNacimiento == '' || req.body.Correo == '') {
-                res.status(400).json({"message": "Paciente no encontrado"});
-            }
 
             let results = await PacienteModel.insert(data);
             
             if (results.affectedRows > 0) {
-                res.status(201).json({"message": "Paciente insertado correctamente"});
+                return res.status(201).json({"message": "Paciente insertado correctamente"});
             }
         } catch (err) {
             res.status(500).json({"message": "Error al insertar paciente"});
@@ -68,16 +65,13 @@ export class PacienteController {
                 req.body.Correo,
                 req.params.id
             ]
-            if (req.body.DNI == '' || req.body.Nombre == '' || req.body.Direccion == '' || req.body.CodigoPostal == '' || req.body.Telefono == '' || req.body.Genero == '' || req.body.FechaNacimiento == '' || req.body.Correo == '') {
-                res.status(400).json({"message": "Campos obligatorios vacios"});
-            } 
             
             let results = await PacienteModel.update(data);
             
             if (results.affectedRows == 0) {
-                res.status(404).json({"message": "Paciente no encontrado"});
+                return res.status(404).json({"message": "Paciente no encontrado"});
             } else if (results.affectedRows > 0) {
-                res.status(200).json({"message": "Paciente actualizado correctamente"});
+                return res.status(200).json({"message": "Paciente actualizado correctamente"});
             }
         } catch (err) {
             res.status(500).json({"message": "Error al actualizar paciente"});
@@ -90,9 +84,9 @@ export class PacienteController {
             let data = [req.params.id];
             let results = await PacienteModel.delete(data);
             if (results.affectedRows == 0) {
-                res.status(404).json({"message": "Paciente no encontrado"});
+                return res.status(404).json({"message": "Paciente no encontrado"});
             } else if (results.affectedRows > 0) {
-                res.status(204).send();
+                return res.status(204).send();
             }
         } catch (err) {
             res.status(500).json({"message": "Error al eliminar paciente"});
