4a6a849b8c99fecb94dc485bea76b4bc30909e6f
OurScorpion045 <sams24484@gmail.com>
2026-09-01 11:27:45 -0700

    Update: File errorHandler.js

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,11 @@
+export const errorHandler = (err, req, res, next) => {
+    console.error(err);
+
+    const statusCode = err.statusCode || 500;
+
+    const message = statusCode === 500 ? "Internal Server Error" : err.message;
+
+    res.status(statusCode).json({
+        error: message
+    });
+}
