4e43853683cd3e516df37feeb6a4ab7f89a9371b
OurScorpion045 <sams24484@gmail.com>
2026-09-03 11:00:56 -0700

    Update: File src/middleware/validation/validateUsuario.js

--- src/middleware/validation/validateUsuario.js
+++ src/middleware/validation/validateUsuario.js
@@ -1,13 +1,20 @@
+import { pool } from "../../config/Database.js";
+import { ValidationError } from "../../errors/ValidationError.js";
 import { usuarioSchema } from "../../schemas/usuario.schema.js";
+import { ConflictError } from "../../errors/ConflictError.js";
 
-export const validateUsuario = (req, res, next) => {
+export const validateUsuario = async (req, res, next) => {
     const validate = usuarioSchema.safeParse(req.body);
 
     if (!validate.success) {
-        return res.status(400).json({
-            error: "Datos invalidos",
-            details: validate.error.issues
-        });
+        throw new ValidationError("Datos inválidos");
+    }
+
+    const correos = await pool.query("SELECT Usuario FROM usuarios");
+    const usuarios = correos[0].map(usuario => usuario.Usuario);
+
+    if (usuarios.includes(req.body.Usuario)) {
+        throw new ConflictError("Un usuario ya existente con esa direccion de correo electronico");
     }
 
     next();
