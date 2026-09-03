21a21d45f79f9e4be546722e240fd58a732f80b2
OurScorpion045 <sams24484@gmail.com>
2026-09-03 10:43:32 -0700

    Update: File PacienteController.js

--- src/controllers/PacienteController.js
+++ src/controllers/PacienteController.js
@@ -1,96 +1,77 @@
 import { PacienteModel } from "../models/PacienteModel.js";
+import { NotFoundError } from "../errors/NotFoundError.js";
+import { AppError } from "../errors/AppError.js";
 
 export class PacienteController {
 
     static async getAll(req, res) {
-        try {
-            let results = await PacienteModel.getAll();
-            return res.json(results);
-        } catch (err) {
-            res.status(500).json({"message": "Error al seleccionar pacientes"});
-            console.error(err);
-        }
-
+        let results = await PacienteModel.getAll();
+        return res.json(results);
     }
 
     static async getById(req, res) {
-        try {
-            let data = [req.params.id];
-            let results = await PacienteModel.getById(data);
-            if (JSON.stringify(results) === '[]') {
-                return res.status(404).json({"message": "Paciente no encontrado"});
-            } else {
-                return res.status(200).json(results);
-            }
-        } catch (err) {
-            res.status(500).json({"message": "Error al seleccionar paciente"});
-            console.error(err);
+        let data = [req.params.id];
+        let results = await PacienteModel.getById(data);
+
+        if (results.length == 0) {
+            throw new NotFoundError("Paciente no encontrado");
+        } else {
+            return res.status(200).json(results);
         }
     }
 
-    static async insert(req, res) {
-        try {
-            let data = [
-                req.body.DNI,
-                req.body.Nombre,
-                req.body.Direccion,
-                req.body.CodigoPostal,
-                req.body.Telefono,
-                req.body.Genero,
-                req.body.FechaNacimiento,
-                req.body.Correo
-            ]
+    static async insert(req, res, next) {
+        let data = [
+            req.body.DNI,
+            req.body.Nombre,
+            req.body.Direccion,
+            req.body.CodigoPostal,
+            req.body.Telefono,
+            req.body.Genero,
+            req.body.FechaNacimiento,
+            req.body.Correo
+        ]
 
-            let results = await PacienteModel.insert(data);
-            
-            if (results.affectedRows > 0) {
-                return res.status(201).json({"message": "Paciente insertado correctamente"});
-            }
-        } catch (err) {
-            res.status(500).json({"message": "Error al insertar paciente"});
-            console.error(err);
+        let results = await PacienteModel.insert(data);
+        
+        if (results.affectedRows == 0) {
+            throw new AppError("Error al insertar paciente");
         }
+
+        return res.status(201).json({"message": "Paciente insertado correctamente"});
+
     }
 
     static async update(req, res) {
-        try {
-            let data = [
-                req.body.DNI,
-                req.body.Nombre,
-                req.body.Direccion,
-                req.body.CodigoPostal,
-                req.body.Telefono,
-                req.body.Genero,
-                req.body.FechaNacimiento,
-                req.body.Correo,
-                req.params.id
-            ]
-            
-            let results = await PacienteModel.update(data);
-            
-            if (results.affectedRows == 0) {
-                return res.status(404).json({"message": "Paciente no encontrado"});
-            } else if (results.affectedRows > 0) {
-                return res.status(200).json({"message": "Paciente actualizado correctamente"});
-            }
-        } catch (err) {
-            res.status(500).json({"message": "Error al actualizar paciente"});
-            console.error(err);
+        let data = [
+            req.body.DNI,
+            req.body.Nombre,
+            req.body.Direccion,
+            req.body.CodigoPostal,
+            req.body.Telefono,
+            req.body.Genero,
+            req.body.FechaNacimiento,
+            req.body.Correo,
+            req.params.id
+        ]
+        
+        let results = await PacienteModel.update(data);
+        
+        if (results.affectedRows == 0) {
+            throw new NotFoundError("Paciente no encontrado");
         }
+        
+        return res.status(200).json({"message": "Paciente actualizado correctamente"});
     }
 
     static async delete(req, res) {
-        try {
-            let data = [req.params.id];
-            let results = await PacienteModel.delete(data);
-            if (results.affectedRows == 0) {
-                return res.status(404).json({"message": "Paciente no encontrado"});
-            } else if (results.affectedRows > 0) {
-                return res.status(204).send();
-            }
-        } catch (err) {
-            res.status(500).json({"message": "Error al eliminar paciente"});
-            console.error(err);
+        let data = [req.params.id];
+        let results = await PacienteModel.delete(data);
+
+        if (results.affectedRows == 0) {
+            throw new NotFoundError("Paciente no encontrado");
         }
+        
+        return res.status(204).send();
     }
 }
