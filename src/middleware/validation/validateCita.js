fbb7b15a2955a0563a99ea79ad7c01a24ffc8fbd
OurScorpion045 <sams24484@gmail.com>
2026-09-03 10:59:32 -0700

    Update: File src/middleware/validation/validateCita.js

--- src/middleware/validation/validateCita.js
+++ src/middleware/validation/validateCita.js
@@ -1,14 +1,12 @@
+import { ValidationError } from "../../errors/ValidationError.js";
 import { citaSchema } from "../../schemas/citas.schema.js";
 
 export const validateCita = (req, res, next) => {
     const validate = citaSchema.safeParse(req.body);
 
     if (!validate.success) {
-        return res.status(400).json({
-            error: "Datos invalidos",
-            details: validate.error.issues
-        });
-    }
+        throw new ValidationError("Datos inválidos");
+    };
 
     next();
 }
