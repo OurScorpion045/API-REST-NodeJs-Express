9b9bc7cb32c960e2b51be7b18a006d83a942646e
OurScorpion045 <sams24484@gmail.com>
2026-09-14 11:05:34 -0700

    Update: File UsuarioRoute.js

--- src/routes/UsuarioRoute.js
+++ src/routes/UsuarioRoute.js
@@ -1,5 +1,6 @@
 import { UsuarioController } from "../controllers/UsuarioController.js";
 import { validateUsuario } from "../middleware/validation/validateUsuario.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 import express from "express";
 
 export const usuarioRouter = express.Router();
@@ -8,22 +9,22 @@ usuarioRouter.post("/login", async (req, res) => {
     await UsuarioController.login(req, res);
 })
 
-usuarioRouter.get("/usuarios", async (req, res) => {
+usuarioRouter.get("/usuarios", authMiddleware, async (req, res) => {
     await UsuarioController.getAll(req, res);
 });
 
-usuarioRouter.get("/usuarios/:id", async (req, res) => {
+usuarioRouter.get("/usuarios/:id", authMiddleware, async (req, res) => {
     await UsuarioController.getById(req, res);
 });
 
-usuarioRouter.post("/usuarios", validateUsuario, async (req, res) => {
+usuarioRouter.post("/usuarios", authMiddleware, validateUsuario, async (req, res) => {
     await UsuarioController.insert(req, res);
 });
 
-usuarioRouter.put("/usuarios/:id", validateUsuario, async (req, res) => {
+usuarioRouter.put("/usuarios/:id", authMiddleware, validateUsuario, async (req, res) => {
     await UsuarioController.update(req, res);
 });
 
-usuarioRouter.delete("/usuarios/:id", async (req, res) => {
+usuarioRouter.delete("/usuarios/:id", authMiddleware, async (req, res) => {
     await UsuarioController.delete(req, res);
 });
