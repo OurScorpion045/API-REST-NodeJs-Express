7e6c34032f095a56d512696ba9a6147093a52e12
OurScorpion045 <sams24484@gmail.com>
2026-09-14 11:06:23 -0700

    Correction: File app.js

--- src/app.js
+++ src/app.js
@@ -3,21 +3,12 @@ import { pacientesRouter } from "./routes/PacienteRoute.js";
 import { citasRouter } from "./routes/CitasRoute.js";
 import { usuarioRouter } from "./routes/UsuarioRoute.js";
 import { errorHandler } from "./middleware/errorHandler.js";
-import { createToken } from "../test/helpers/createToken.js";
 
 export const app = express();
 app.use(express.json());
 app.use(pacientesRouter);
 app.use(citasRouter);
 app.use(usuarioRouter);
-
-app.post("/test-token", async (req, res) => {
-    let token = await createToken();
-
-    res.json({
-        token: `${token}`
-    });
-})
 
 app.use(errorHandler);
 
