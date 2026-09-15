7e4f21cb9d553173cdb25258e5e0aa41ea459bc1
OurScorpion045 <sams24484@gmail.com>
2026-09-15 12:19:06 -0700

    Update: File UsuarioModel.js

--- src/models/UsuarioModel.js
+++ src/models/UsuarioModel.js
@@ -3,7 +3,7 @@ import { pool } from "../config/Database.js";
 export class UsuarioModel {
 
     static async getAll() {
-        const sql = "SELECT `Usuario`, `Estado` FROM `usuarios` ORDER BY `UsuarioId` DESC";
+        const sql = "SELECT `Usuario`, `Estado`, `Rol` FROM `usuarios` ORDER BY `UsuarioId` DESC";
         const [rows, fields] = await pool.execute(sql);
         return rows;
     }
@@ -21,13 +21,13 @@ export class UsuarioModel {
     }
 
     static async insert(usuarioData) {
-        const sql = "INSERT INTO `usuarios`(`Usuario`, `Password`, `Estado`) VALUES (?, ?, ?)";
+        const sql = "INSERT INTO `usuarios`(`Usuario`, `Password`, `Estado`, `Rol`) VALUES (?, ?, ?, ?)";
         const [rows, fields] = await pool.execute(sql, usuarioData);
         return rows;
     }
 
     static async update(usuarioData) {
-        const sql = "UPDATE `usuarios` SET `Usuario` = ?, `Password` = ?, `Estado` = ? WHERE `UsuarioId` = ?";
+        const sql = "UPDATE `usuarios` SET `Usuario` = ?, `Password` = ?, `Estado` = ?, `Rol` = ? WHERE `UsuarioId` = ?";
         const [rows, fields] = await pool.execute(sql, usuarioData);
         return rows;
     }
