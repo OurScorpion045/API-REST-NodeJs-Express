519487d4a74f30f66dff81ef1bd33db090df439b
OurScorpion045 <sams24484@gmail.com>
2026-09-17 12:04:28 -0700

    Update: File CitasRoute.js

--- src/routes/CitasRoute.js
+++ src/routes/CitasRoute.js
@@ -1,26 +1,27 @@
 import { CitasController } from "../controllers/CitasController.js";
 import { validateCita } from "../middleware/validation/validateCita.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
+import { authorizationMiddleware } from "../middleware/authorizationMiddleware.js";
 import express from "express";
 
 export const citasRouter = express.Router();
 
-citasRouter.get("/citas", authMiddleware, async (req, res) => {
+citasRouter.get("/citas", authMiddleware, authorizationMiddleware("USER", "ADMIN"), async (req, res) => {
     await CitasController.getAll(req, res);
 });
 
-citasRouter.get("/citas/:id", authMiddleware, async (req, res) => {
+citasRouter.get("/citas/:id", authMiddleware, authorizationMiddleware("USER", "ADMIN"), async (req, res) => {
     await CitasController.getById(req, res);
 });
 
-citasRouter.post("/citas", authMiddleware, validateCita, async (req, res) => {
+citasRouter.post("/citas", authMiddleware, authorizationMiddleware("USER", "ADMIN"), validateCita, async (req, res) => {
     await CitasController.insert(req, res);
 });
 
-citasRouter.put("/citas/:id", authMiddleware, validateCita, async (req, res) => {
+citasRouter.put("/citas/:id", authMiddleware, authorizationMiddleware("USER", "ADMIN"), validateCita, async (req, res) => {
     await CitasController.update(req, res);
 });
 
-citasRouter.delete("/citas/:id", authMiddleware, async (req, res) => {
+citasRouter.delete("/citas/:id", authorizationMiddleware("USER", "ADMIN"), authMiddleware, async (req, res) => {
     await CitasController.delete(req, res);
 });
