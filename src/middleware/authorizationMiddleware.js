6990f84ba5084783800ad9bf8db7dc81ac7854bd
OurScorpion045 <sams24484@gmail.com>
2026-09-17 12:04:03 -0700

    Update: File authorizationMiddleware.js

---
+++ src/middleware/authorizationMiddleware.js
@@ -0,0 +1,12 @@
+import { ForbiddenError } from "../errors/ForbiddenError.js";
+
+export const authorizationMiddleware = (...rolesPermitidos) => {
+
+    return (req, res, next) => {
+        if (rolesPermitidos.includes(req.usuario.Rol)) {
+            next();
+        } else {
+            throw new ForbiddenError("Acceso denegado");
+        }
+    }
+}
