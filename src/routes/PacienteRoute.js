b772f6026730421d37bd8ddcda15e5d05e8b9473
OurScorpion045 <sams24484@gmail.com>
2026-09-17 12:04:53 -0700

    Update: File PacienteRoute.js

--- src/routes/PacienteRoute.js
+++ src/routes/PacienteRoute.js
@@ -2,25 +2,26 @@ import { PacienteController } from "../controllers/PacienteController.js";
 import express from "express";
 import { validatePaciente } from "../middleware/validation/validatePaciente.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
+import { authorizationMiddleware } from "../middleware/authorizationMiddleware.js";
 
 export const pacientesRouter = express.Router();
 
-pacientesRouter.get("/pacientes", authMiddleware, async (req, res) => {
+pacientesRouter.get("/pacientes", authMiddleware, authorizationMiddleware("USER", "ADMIN"), async (req, res) => {
     await PacienteController.getAll(req, res);
 });
 
-pacientesRouter.get("/pacientes/:id", authMiddleware, async (req, res) => {
+pacientesRouter.get("/pacientes/:id", authMiddleware, authorizationMiddleware("USER", "ADMIN"), async (req, res) => {
     await PacienteController.getById(req, res);
 });
 
-pacientesRouter.post("/pacientes", authMiddleware, validatePaciente, async (req, res) => {
+pacientesRouter.post("/pacientes", authMiddleware, authorizationMiddleware("USER", "ADMIN"), validatePaciente, async (req, res) => {
     await PacienteController.insert(req, res);
 });
 
-pacientesRouter.put("/pacientes/:id", authMiddleware, validatePaciente, async (req, res) => {
+pacientesRouter.put("/pacientes/:id", authMiddleware, authorizationMiddleware("USER", "ADMIN"), validatePaciente, async (req, res) => {
     await PacienteController.update(req, res);
 });
 
-pacientesRouter.delete("/pacientes/:id", authMiddleware, async (req, res) => {
+pacientesRouter.delete("/pacientes/:id", authMiddleware, authorizationMiddleware("USER", "ADMIN"), async (req, res) => {
     await PacienteController.delete(req, res);
 });
