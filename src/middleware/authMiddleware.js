6740564da516d09c2158f4191b88d2a0e86b8fd0
OurScorpion045 <sams24484@gmail.com>
2026-09-24 10:26:50 -0700

    Update: File authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -15,7 +15,7 @@ export const authMiddleware = (req, res, next) => {
     }
 
     try {
-        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        const payload = jwt.verify(token, process.env.JWT_SECRET, { algorithms: ["HS256"] });
 
         req.usuario = payload;
 
